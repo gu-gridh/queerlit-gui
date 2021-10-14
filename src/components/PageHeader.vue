cdbd1499f9b531be4bc017e317b43de8c127e7d3
Arild Matsson <arild.matsson@gu.se>
2021-10-14 19:08:46 +0200

    Site menu

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,11 +1,16 @@
 <template>
   <header class="bg-pink-600 text-white">
-    <div class="container max-w-screen-md py-8">
-      <router-link to="/">
+    <div
+      class="container max-w-screen-md py-8 flex justify-between items-center"
+    >
+      <router-link to="/" class="flex-1">
         <h1 class="text-3xl">
           <img src="@/assets/queerlit-rocksalt-w.svg" class="h-16" />
         </h1>
       </router-link>
+      <div class="text-lg">
+        <a href="https://queerlit.dh.gu.se/om/" class="ml-4">Om</a>
+      </div>
     </div>
   </header>
 </template>
