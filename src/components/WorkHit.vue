01328bfb1ced2723caa7e56b06fc902638e08756
Arild Matsson <arild.matsson@gu.se>
2022-01-21 18:21:38 +0100

    Simpler result list

--- src/components/WorkHit.vue
+++ src/components/WorkHit.vue
@@ -1,34 +1,15 @@
 <template>
   <router-link :to="`/verk/${id}`" class="group">
-    <article class="block clearfix mb-2">
-      <div
-        class="
-          float-right
-          ml-2
-          mb-1
-          h-48
-          w-32
-          flex
-          justify-center
-          items-center
-          p-1
-        "
-      >
-        <img
-          src="@/assets/drottningens-juvelsmycke.jpeg"
-          class="max-h-full border"
-        />
-      </div>
-      <h3 class="text-xl group-hover:underline">
+    <article class="block mb-6 items-baseline">
+      <h3 class="w-1/2 my-2 text-xl group-hover:underline">
         {{ title }}
       </h3>
-      <div class="flex my-2 text-lg">
-        <div class="w-1/2 pr-4">
-          {{ creators?.join(", ") }}
+      <div class="my-2 flex">
+        <div v-for="creator in creators" :key="creator" class="mr-4">
+          {{ creator }}
         </div>
-        <div class="w-1/4 pr-4">Bok</div>
-        <div class="w-1/4 pr-4">{{ date }}</div>
       </div>
+      <div class="my-2">{{ date }}</div>
       <div class="my-2">
         <Term v-for="term in terms" :key="term" class="mr-1 mb-1">
           {{ term }}
