a48ef29c3f9a21eb7122c05b2a3fc466beaf0cce
Arild Matsson <arild.matsson@gu.se>
2022-03-01 17:15:33 +0100

    Lint and format scripts
    https://vueschool.io/articles/vuejs-tutorials/eslint-and-prettier-with-vite-and-vue-js-3/
    https://eslint.vuejs.org/user-guide/#does-not-work-well-with-script-setup
    https://stackoverflow.com/a/69806781/1750365

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
   env: {
     node: true,
+    "vue/setup-compiler-macros": true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-	    "**prettier**"
-
-  ],
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-  }
-}
+    // https://eslint.vuejs.org/user-guide/#does-not-work-well-with-script-setup
+    "vue/script-setup-uses-vars": "error",
+  },
+};
