fc60d4801a49e9d2291640ca42e281abd56eb6d4
Arild Matsson <arild.matsson@gu.se>
2022-07-08 15:36:30 +0200

    Query store module

---
+++ src/search/query.store.js
@@ -0,0 +1,26 @@
+export default {
+  state: {
+    text: "",
+    terms: [],
+    title: "",
+    author: null,
+    yearStart: null,
+    yearEnd: null,
+    genreform: null,
+  },
+  mutations: {
+    setQuery(
+      state,
+      { text, terms, title, author, yearStart, yearEnd, genreform }
+    ) {
+      // Modify each value only if it is given.
+      if (text !== undefined) state.text = text;
+      if (terms !== undefined) state.terms = terms;
+      if (title !== undefined) state.title = title;
+      if (author !== undefined) state.author = author;
+      if (yearStart !== undefined) state.yearStart = parseInt(yearStart);
+      if (yearEnd !== undefined) state.yearEnd = parseInt(yearEnd);
+      if (genreform !== undefined) state.genreform = genreform;
+    },
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,19 @@
 import { createStore } from "vuex";
 import { search } from "@/services/libris.service";
+import query from "@/search/query.store";
 
 export default createStore({
-  state() {
-    return {
-      query: {
-        text: "",
-        terms: [],
-        title: "",
-        author: null,
-        yearStart: null,
-        yearEnd: null,
-        genreform: "",
-      },
-      results: null,
-      total: 0,
-      sort: "-publication.year",
-      offset: 0,
-      currentSearch: null,
-    };
+  modules: {
+    query,
+  },
+  state: {
+    results: null,
+    total: 0,
+    sort: "-publication.year",
+    offset: 0,
+    currentSearch: null,
   },
   mutations: {
-    setQuery(
-      state,
-      { text, terms, title, author, yearStart, yearEnd, genreform }
-    ) {
-      if (text !== undefined) state.query.text = text;
-      if (terms) state.query.terms = terms;
-      if (title !== undefined) state.query.title = title;
-      if (author !== undefined) state.query.author = author;
-      if (yearStart !== undefined) state.query.yearStart = parseInt(yearStart);
-      if (yearEnd !== undefined) state.query.yearEnd = parseInt(yearEnd);
-      if (genreform !== undefined) state.query.genreform = genreform;
-    },
     setSearching(state, query) {
       state.currentSearch = query ? JSON.stringify(query) : null;
     },
