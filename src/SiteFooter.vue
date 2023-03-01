a440d6cd2bc1d268ad12c73e92059feca24e8955
Arild Matsson <arild.matsson@gu.se>
2023-03-01 13:28:27 +0100

    Add CC0 license in footer

--- src/SiteFooter.vue
+++ src/SiteFooter.vue
@@ -3,12 +3,20 @@
 <template>
   <div class="container mt-8 py-8 border-t flex gap-4 justify-between text-sm">
     <div class="flex-1 q-body">
-      Queerlitprojektet bedrivs på
-      <a href="https://gu.se">Göteborgs universitet</a>, genom samarbeten med
-      bland annat <a href="http://www2.ub.gu.se/kvinn/">KvinnSam</a>,
-      <a href="https://gu.se/digital-humaniora">GRIDH</a>,
-      <a href="https://www.sh.se/">Södertörns högskola</a> och
-      <a href="https://lnu.se/">Linnéuniversitet</a>.
+      <div>
+        Queerlitprojektet bedrivs på
+        <a href="https://gu.se">Göteborgs universitet</a>, genom samarbeten med
+        bland annat <a href="http://www2.ub.gu.se/kvinn/">KvinnSam</a>,
+        <a href="https://gu.se/digital-humaniora">GRIDH</a>,
+        <a href="https://www.sh.se/">Södertörns högskola</a> och
+        <a href="https://lnu.se/">Linnéuniversitet</a>.
+      </div>
+      <div>
+        Licens:
+        <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.sv"
+          >CC0</a
+        >
+      </div>
     </div>
     <a href="https://gu.se" class="w-20"><img src="@/assets/gu.svg" /></a>
   </div>
