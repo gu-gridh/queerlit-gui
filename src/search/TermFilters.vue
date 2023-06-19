9d811b7ab63be22d5ebf64b83f887459e02913c5
Arild Matsson <arild.matsson@gu.se>
2023-06-19 13:57:32 +0200

    Collections before comboboxes

--- src/search/TermFilters.vue
+++ src/search/TermFilters.vue
@@ -9,27 +9,29 @@ const { terms, add, remove, termsSecondary, addSecondary, removeSecondary } =
 </script>
 
 <template>
-  <Labeled label="Ämnesord" for-id="terms">
-    <TermCombobox
-      :terms="terms"
-      input-id="terms"
-      help="Sök efter ord från QLIT, Queerlits ämnesordslista, som är centrala i verket"
-      class="mb-4"
-      @add="add"
-      @remove="remove"
-    />
-  </Labeled>
+  <Labeled label="Ämnen">
+    <CollectionsSmall />
 
-  <Labeled label="Perifera ämnesord" for-id="terms-secondary">
-    <TermCombobox
-      :terms="termsSecondary"
-      input-id="terms-secondary"
-      help="Sök efter ord från QLIT, Queerlits ämnesordslista, som är perifera i verket"
-      class="mb-4"
-      @add="addSecondary"
-      @remove="removeSecondary"
-    />
-  </Labeled>
+    <Labeled label="Centrala ämnesord" for-id="terms">
+      <TermCombobox
+        :terms="terms"
+        input-id="terms"
+        help="Sök efter ord från QLIT, Queerlits ämnesordslista, som är centrala i verket"
+        class="mb-4"
+        @add="add"
+        @remove="remove"
+      />
+    </Labeled>
 
-  <CollectionsSmall />
+    <Labeled label="Perifera ämnesord" for-id="terms-secondary">
+      <TermCombobox
+        :terms="termsSecondary"
+        input-id="terms-secondary"
+        help="Sök efter ord från QLIT, Queerlits ämnesordslista, som är perifera i verket"
+        class="mb-4"
+        @add="addSecondary"
+        @remove="removeSecondary"
+      />
+    </Labeled>
+  </Labeled>
 </template>
