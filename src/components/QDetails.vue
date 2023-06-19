8ba39d26c5335d637cecd23f85305fe83c79db2e
Arild Matsson <arild.matsson@gu.se>
2023-06-19 20:11:51 +0200

    Hide some search fields

---
+++ src/components/QDetails.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  heading: {
+    type: String,
+    required: true,
+  },
+  expanded: Boolean,
+});
+defineEmits(["toggle"]);
+</script>
+
+<template>
+  <div class="border-2 border-dashed border-gray-300 px-2 rounded-lg">
+    <header class="text-lg my-1 cursor-pointer" @click="$emit('toggle')">
+      <icon v-if="expanded" icon="minus" size="sm" class="opacity-70" />
+      <icon v-else icon="plus" size="sm" class="opacity-70" />
+      {{ heading }}
+    </header>
+
+    <div v-if="expanded">
+      <slot />
+    </div>
+  </div>
+</template>
--- src/search/SearchForm.vue
+++ src/search/SearchForm.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="bg-white rounded-xl shadow-lg" @keyup.enter="doSearch">
-    <div class="py-6 px-6">
-      <div class="mb-4 q-body">
-        <ReadMore :expanded="isQueryEmpty">
-          <p>
-            Queerlit är en databas för svensk skönlitteratur som skildrar
-            samkönat begär och överskridanden av binära könsnormer. Det
-            innefattar bland annat – men inte enbart – skildringar av
-            HBTQI-personer.
-          </p>
-          <p>
-            Databasen är under uppbyggnad, och vi arbetar fortfarande med att
-            föra in all information. Om du saknar något verk, eller har tips på
-            ämnesord till något av verken, så vill vi gärna
-            <a href="https://queerlit.dh.gu.se/om/kontakt" title="Kontakt"
-              >höra av dig</a
-            >!
-          </p>
-        </ReadMore>
-      </div>
-
-      <Freetext />
+  <div class="bg-white rounded-xl shadow-lg p-6" @keyup.enter="doSearch">
+    <div class="mb-4 q-body">
+      <ReadMore :expanded="isQueryEmpty">
+        <p>
+          Queerlit är en databas för svensk skönlitteratur som skildrar samkönat
+          begär och överskridanden av binära könsnormer. Det innefattar bland
+          annat – men inte enbart – skildringar av HBTQI-personer.
+        </p>
+        <p>
+          Databasen är under uppbyggnad, och vi arbetar fortfarande med att föra
+          in all information. Om du saknar något verk, eller har tips på
+          ämnesord till något av verken, så vill vi gärna
+          <a href="https://queerlit.dh.gu.se/om/kontakt" title="Kontakt"
+            >höra av dig</a
+          >!
+        </p>
+      </ReadMore>
     </div>
 
-    <div class="py-4 px-6 border-t border-dashed border-gray-500">
-      <h3 class="text-lg mb-2">Avancerat</h3>
+    <Freetext class="mb-6" />
 
-      <TermFilters />
+    <TermFilters />
 
+    <Labeled label="Utgivningsår" for-id="year">
+      <YearFilter
+        :start="yearStart"
+        :end="yearEnd"
+        input-id="year"
+        @change="yearChange"
+      />
+    </Labeled>
+
+    <QDetails
+      heading="Avancerade sökfilter"
+      class="my-6"
+      :expanded="expandedAdvanced || usingAdvanced"
+      @toggle="toggleAdvanced()"
+    >
       <div class="flex flex-wrap -mx-2">
         <div class="w-full sm:w-1/2 p-2">
           <Labeled label="Titel" for-id="title">
@@ -56,17 +65,6 @@
         </div>
 
         <div class="w-full sm:w-1/2 p-2">
-          <Labeled label="Utgivningsår" for-id="year">
-            <YearFilter
-              :start="yearStart"
-              :end="yearEnd"
-              input-id="year"
-              @change="yearChange"
-            />
-          </Labeled>
-        </div>
-
-        <div class="w-full sm:w-1/2 p-2">
           <Labeled label="Genre/form" for-id="genreform">
             <Autocomplete
               :value="genreform"
@@ -80,9 +78,10 @@
           </Labeled>
         </div>
       </div>
-      <div class="text-center mt-4">
-        <QButton class="text-xl" @click="doSearch">Sök</QButton>
-      </div>
+    </QDetails>
+
+    <div class="text-center mt-4">
+      <QButton class="text-xl" @click="doSearch">Sök</QButton>
     </div>
   </div>
   <div class="text-center mt-4">
@@ -97,6 +96,7 @@
 </template>
 
 <script setup>
+import { useToggle } from "@vueuse/core";
 import useQuery from "@/search/query.composable";
 import { searchGenreform, searchPerson } from "@/services/libris.service";
 import Freetext from "./Freetext.vue";
@@ -108,6 +108,8 @@ import Labeled from "@/components/Labeled.vue";
 import QInput from "@/components/QInput.vue";
 import ReadMore from "@/components/ReadMore.vue";
 import TermFilters from "./TermFilters.vue";
+import QDetails from "@/components/QDetails.vue";
+import { computed } from "vue";
 
 const { doSearch, setQuery } = useSearch();
 const {
@@ -120,6 +122,10 @@ const {
   getGenreformLabel,
   isQueryEmpty,
 } = useQuery();
+const [expandedAdvanced, toggleAdvanced] = useToggle();
+const usingAdvanced = computed(
+  () => title.value || author.value || genreform.value
+);
 
 function setTitle(event) {
   setQuery({ title: event.target.value });
