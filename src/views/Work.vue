fa4de603902ec16b3e1c1e3a89b58d1a4e929d5f
Arild Matsson <arild.matsson@gu.se>
2023-07-19 14:49:49 +0200

    TSify Work view

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,9 +1,4 @@
-<template>
-  <WorkDetails v-if="work" :work="work" />
-  <LoadingSpinner v-else />
-</template>
-
-<script setup>
+<script setup lang="ts">
 import { computed, ref } from "vue";
 import { get } from "@/services/libris.service";
 import { useRoute } from "vue-router";
@@ -11,14 +6,20 @@ import useTitle from "./title.composable";
 import use404 from "./404.composable";
 import WorkDetails from "@/search/WorkDetails.vue";
 import LoadingSpinner from "@/components/LoadingSpinner.vue";
+import type { Work } from "@/types/work";
 
 const route = useRoute();
 const { flag404 } = use404();
 
-const work = ref();
+const work = ref<Work>();
 useTitle(computed(() => work.value && work.value.title));
 
-get(route.params.id)
+get(route.params.id as string)
   .then((work_) => (work.value = work_))
   .catch(flag404);
 </script>
+
+<template>
+  <WorkDetails v-if="work" :work="work" />
+  <LoadingSpinner v-else />
+</template>
