504dd12246e2c9fcb106f76d146dcbdf84d935c0
Arild Matsson <arild.matsson@gu.se>
2023-09-13 09:48:40 +0200

    WIP: Sync from url to store

--- src/search/paramsSearch.composable.ts
+++ src/search/paramsSearch.composable.ts
@@ -1,16 +1,30 @@
 import { watch } from "vue";
-import { useUrlSearchParams } from "@vueuse/core";
+import { useUrlSearchParams, type UrlParams } from "@vueuse/core";
 import useQuery from "./query.composable";
 
-export default function useParamsSearch() {
-  const { text, yearStart, yearEnd, serializedQuery } = useQuery();
+// All strings. Just declare whether multiple or not.
+type Params = UrlParams & {
+  text: string;
+  yearStart: string;
+  yearEnd: string;
+};
 
-  const params = useUrlSearchParams("history", { removeFalsyValues: true });
+// TODO After switching to Pinia, rewrite to make the store use `useUrlSearchParams` directly.
+export default function useParamsSearch() {
+  const { text, yearStart, yearEnd, serializedQuery, setQuery } = useQuery();
+  const params = useUrlSearchParams<Params>("history", {
+    removeFalsyValues: true,
+  });
 
   // Sync from store to url
   watch(
     serializedQuery,
-    () => {
+    (to, from) => {
+      console.log("watch store", to);
+      if (to == from) {
+        console.warn(to, from);
+        return;
+      }
       Object.assign(params, {
         text: text.value,
         yearStart: String(yearStart.value || ""),
@@ -18,5 +32,19 @@ export default function useParamsSearch() {
       });
     },
     { deep: true },
+  );
+
+  // TODO When we do `router.push("/")` we lose the params.
+  watch(
+    params,
+    () => {
+      console.log("watch params", { ...params });
+      setQuery({
+        text: params.text as string | undefined,
+        yearStart: Number(params.yearStart) || null,
+        yearEnd: Number(params.yearEnd) || null,
+      });
+    },
+    { deep: true, immediate: true },
   );
 }
